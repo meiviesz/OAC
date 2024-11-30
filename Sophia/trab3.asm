76466a24258ae81094c3de8594f64d17b1ac7811
Mavi Mourão <mavimmourao@gmail.com>
2024-11-30 18:05:14 -0300

    criando arquivo do trabalho 2

---
+++ Sophia/trab3.asm
@@ -0,0 +1,29 @@
+.data
+m1: .word 1 2 3 4 5 6 7 8 9
+m2: .word 9 8 7 6 5 4 3 2 1
+mr: .word 0 0 0 0 0 0 0 0 0
+
+
+.text
+
+#read_cel(lin, col):
+
+#write_cel(lin, col, val):
+
+
+#soma(m1, m2, mr, lado):
+# 1 elemento da 1 matriz: a0
+# 1 elemento da 2 matriz: a1
+# 1 elemento da matriz resultado: a2
+# largura da matriz: a3
+# soma e coloca em mr (a3)
+
+#multiplica(m1, m2, mr, lado):
+# mesmos parametros da soma
+# matriz mr recebe resultado da mult de m1 por m2
+
+#transposta(m1, mr, lado):
+# coloca o resultado em mr
+
+#imprime(m1, lado):
+# imprime o conteúdo da matriz
